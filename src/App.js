6443f27a9c3d7525f18aca258aa16c2c8c3d4eb0
siazur <asiazur@yandex.com>
2026-02-23 01:10:02 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./styles/main.css";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
-//import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
@@ -16,7 +15,7 @@ import ScrollToTop from "./utils/scrollToTop";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/portfolio">
         <ScrollToTop />
         <Navbar />
         <Routes>
